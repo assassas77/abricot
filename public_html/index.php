253eafd1e496cea1cca12f7e6b450beeb280388f
Assassas77 <assassas77@tools-sgebastion-08.tools.eqiad.wmflabs>
2021-09-07 21:44:16 +0000

    adding code for the format dt / dd in Wikisource

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php header('Content-Type: text/html; charset=utf-8');
 if ($_POST["test"]){
-  $resultat = $_POST["test"]; 
+  $resultat = $_POST["test"];
   $original = $resultat;
 } else {
   $resultat = 'ka-cau bp niSa" liah m tieh ; without
@@ -21,12 +21,62 @@ while ($buffer = fgets($handle)){
 
 fclose($handle);
 
-// remplacement
+// preprocessing
+$resultat = preg_replace("/DICT[a-zA-Z ]*LECT. \r\n[0-9]* \r\n/u", "", $resultat);
+
+// remplacement à partir du fichier externe
 foreach ($data as $row){
   $pattern = $row[0];
   $substitute = $row[1];
   $resultat = str_replace($pattern, $substitute, $resultat);
 }
+
+// traitement des retours à la ligne
+$resultat = str_replace("\r\n", " ", $resultat);
+$resultat = str_replace("; ", ";\r\n: ", $resultat);
+$resultat = str_replace(". ", ".\r\n; ", $resultat);
+$resultat = str_replace(" .", ".", $resultat);
+	//traitement des ?
+$resultat = preg_replace("/\r\n;(.+)\? (.+)\? /u", "\r\n;$1?\r\n: $2?\r\n; ", $resultat);
+$resultat = preg_replace("/\r\n:(.+)\? (.+)\r\n/u", "\r\n:$1?\r\n; $2\r\n", $resultat);
+
+	// traitement des , et des ; qui se suivent non reconnus
+	// si deux lignes commencent par le même symbole, il faut soit :
+	//	: .+ , .+;
+	//	: .+
+	// sera remplacé par
+	//	: .+.
+	//	; .+
+	//	: .+
+	// et si il y a un point virgule
+	//	: .+ ;
+	//	: .+
+	// sera remplacé par
+	//	: .+; .+
+for ($i = 1; $i <= 5; $i++){
+	$resultat = preg_replace("/\r\n:(.+),(.+);\r\n:(.+)/u", "\r\n:$1.\r\n;$2;\r\n:$3", $resultat);
+}
+
+$max_def_per_entry = 10;
+for ($i = 1; $i <= $max_def_per_entry; $i++){
+	$resultat = preg_replace("/\r\n:(.+);\r\n:(.+)\r\n/u", "\r\n:$1; $2\r\n", $resultat);
+}
+
+$resultat = str_replace("  ", " ", $resultat);
+
+	// retrait de la dernière ligne vide
+$resultat = str_replace("\r\n;  ", "", $resultat);
+
+	// ajout des section
+$resultat = preg_replace("/— ([a-zA-Z']*) —/u", "<section end=\"\" />\r\n<section begin=\"\" />\r\n=== $1 ===\r\n;", $resultat);
+$resultat = str_replace("\r\n; <section end=\"\" />", "<section end=\"\" />", $resultat);
+	// retrait du header
+$resultat = preg_replace("/DICT[a-zA-Z ]*LECT. \r\n[0-9][0-9]*/u", "", $resultat);
+$resultat = preg_replace("/[0-9]+ DICT[a-zA-Z ]*LECT./u", "", $resultat);
+
+// fin du traitement
+	// retrait des doubles blancs
+$resultat = str_replace("  ", " ", $resultat);
 ?>
 <html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -37,10 +87,10 @@ foreach ($data as $row){
   <textarea name="resultat"><?php echo $resultat;?></textarea>
   <input type="submit" value="ABRICOT!">
 </form>
-</body>
-</html>
 <?php
 foreach ($data as $row){
   echo "<input type='text' value='".htmlspecialchars($row[0],ENT_QUOTES)."'>=><input type='text' value='".htmlentities($row[1])."'><br/>";
 }
 ?>
+</body>
+</html>
